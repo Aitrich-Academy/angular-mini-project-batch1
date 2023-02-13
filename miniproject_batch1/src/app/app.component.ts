98cf845a83344e6aa9fdf34ef7be3be2c54260df
haripriya s <haripriyas4324@gmail.com>
2023-02-13 11:07:09 +0530

    hii guys

--- miniproject_batch1/src/app/app.component.ts
+++ miniproject_batch1/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project';
+  isMenuScrolled=false;
+  @HostListener('window:scroll')
+  scrollCheck(){
+    if(window.pageYOffset>400){
+      this.isMenuScrolled=true;
+    }
+    else
+    {
+      this.isMenuScrolled=false;
+    }
+  }
 }
